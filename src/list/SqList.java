4ae2e632afe35591b3d1499b2d545c14b0bfe06c
yunfeichen119 <yunfeichen119@163.com>
2020-03-24 09:58:01 +0800

    sqList

--- src/list/SqList.java
+++ src/list/SqList.java
@@ -8,37 +8,79 @@ import java.lang.reflect.Array;
 
 public class SqList<T> {
 
-    //public static final Integer MAX = 1024;
-
     private T[] data;
 
-    private Integer size;
+    private int maxSize;
+
+    private int length;
 
     @SuppressWarnings("unchecked")
-    public SqList(Class<T> type, int size){
-        this.data = (T[]) Array.newInstance(type, size);
-        this.size = size;
-    }
-
-    public void put(int index, T item){
-        data[index] = item;
+    public SqList(Class<T> type, int maxSize){
+        this.data = (T[]) Array.newInstance(type, maxSize);
+        this.maxSize = maxSize;
+        this.length = 0;
     }
 
     public T get(int index){
+        if(index < 0 || index >= length){
+            throw new RuntimeException("out of range");
+        }
         return data[index];
     }
 
-    public T[] getAll(){
-        return data;
+
+    public Integer locate(T item){
+        for(int i = 0; i < length; i++){
+            if(item.equals(data[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void insert(int index, T item){
+
+        if(length >= maxSize - 1){
+            throw new RuntimeException("out of range");
+        }
+        if(index > length){
+            throw new RuntimeException("out of range");
+        }
+        if(index == length){
+            data[index] = item;
+        }else{
+            for(int i = length -1 ; i >= index; i--){
+                data[i+1] = data[i];
+            }
+            data[index] = item;
+        }
+        length++;
     }
 
 
+    public void delete(int index){
+        if(index < 0 || index > length -1){
+            throw new RuntimeException("out of range");
+        }
+        for(int i = index; i < length -1; i++){
+            data[i] = data[i+1];
+        }
+        length--;
+    }
+
+
+
     public static void main(String[] args){
-        SqList<Integer> integerSqList = new SqList<Integer>(Integer.class, 1024);
+        SqList<Integer> integerSqList = new SqList<Integer>(Integer.class, 10);
 
-        integerSqList.put(0, 99);
+        for(int i = 5; i >= 0; i--){
+            integerSqList.insert(0, i);
+        }
 
-        Integer[] arr =  integerSqList.getAll();
-        System.out.println(arr);
+        Integer item = integerSqList.locate(3);
+        System.out.println(item);
+
+        integerSqList.delete(3);
+        System.out.println(integerSqList);
     }
 }
